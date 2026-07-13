8e5fa73b3f74f4d3f3248c5504b926f339a586b4
srujan214 <srujannaik214@gmail.com>
2026-07-13 06:58:52 +0530

    updated the StudentDto and StudentSaveDTO classes

---
+++ StudentManagementApp/src/main/java/com/example/StudentManagementApp/StudentDTO/StudentSaveDTO.java
@@ -0,0 +1,19 @@
+package com.example.StudentManagementApp.StudentDTO;
+
+public class StudentSaveDTO {
+    private String name;
+    private String email;
+    private String department;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
+
+
+
